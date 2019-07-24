6522876d92b6ee5f805f1663a97b383c35fdaf14
Chris Sheppard <chrissheppard41@gmail.com>
2019-07-24 22:48:21 +0100

    added unit tests for the reducer

--- src/build.tsx
+++ src/build.tsx
@@ -1,4 +1,4 @@
 import FormBuilder from './js/FormBuilder';
 
 export default FormBuilder;
-//extra exports use `export {...}`
+//extra exports use `export {...}` such as the validation calls, panels, inputs etc
