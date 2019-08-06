2a48c9c0fc28e8d3e217387835a2fa77d97e5e10
csheppard <christopher_sheppard@rapid7.com>
2019-08-06 07:49:30 +0100

    Adding component files for reference

---
+++ examples/components/Toggle.tsx
@@ -0,0 +1,64 @@
+import {Div, Label, Input, useStateValue, inputType, ValidationRules} from "react-form-builder";
+import useValidationSelect from './useValidationSelect';
+import React, {useState} from "react";
+
+const Toggle = ({
+                  label,
+                  id,
+                  inputName,
+                  inputValue,
+                  inputClassName,
+                  validation,
+                  disableChild
+                }: inputType) => {
+  const { actions }: any = useStateValue();
+  const [val, setVal] = useState(inputValue);
+  const { selectedMessage, selected } = useValidationSelect(
+    validation,
+    val,
+    id,
+    actions.addValidation,
+    actions.removeValidation
+  );
+  if (!disableChild) {
+    actions.enableChildren(id, val);
+  }
+
+  return (
+    <Div className="toggleInput">
+      <Label htmlFor={inputName}>
+        {label}
+      </Label>
+      <Input
+        type="checkbox"
+        name={inputName}
+        className={`${inputClassName} ${selected}`}
+        id={id}
+        value={val}
+        onChange={e => setVal(e.target.value)}
+      />
+      <ValidationRules
+        validation={[selectedMessage]}
+      />
+    </Div>
+  );
+};
+
+Toggle.defaultProps = {
+  id: "",
+  inputName: "",
+  inputValue: "",
+  inputClassName: "",
+  validation: {},
+  connected: "",
+  enableChildren: false,
+  disableChild: false,
+  panelName: "",
+  parentClassName: "",
+  label: "",
+  type: "",
+  inputType: "",
+  fromPanel: false
+};
+
+export default Toggle;
---
+++ examples/components/useValidationSelect.tsx
@@ -0,0 +1,32 @@
+interface ValidationHook {
+  selectedMessage: string;
+  selected: boolean;
+}
+
+const useValidationSelect = (
+  validation: any,
+  value: string,
+  id: string,
+  addValidation: (id: string, validationRule: string) => void,
+  removeValidation: (id: string, validationRule: string) => void
+): ValidationHook => {
+  let selectedMessage = "";
+  let selected = false;
+
+  if (validation && validation.required) {
+    selected = validation.selected.enabled;
+
+    if (selected) {
+      if (!value) {
+        selectedMessage = validation.selected.message;
+        addValidation(id, "selected");
+      } else {
+        removeValidation(id, "selected");
+      }
+    }
+  }
+
+  return { selected, selectedMessage };
+};
+
+export default useValidationSelect;
